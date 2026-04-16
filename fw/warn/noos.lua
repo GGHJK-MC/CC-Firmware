fe9996a3d5b9b2a3031e64333385f2182d8e6ef6
SlientPickaxe <Noobsapiens12@outlook.com>
2026-04-16 16:17:46 +0000

    včř

---
+++ fw/warn/noos.lua
@@ -0,0 +1,29 @@
+local i, nOpen = 1, nil
+repeat
+    local name, val = debug.getupvalue(fs.open, i)
+    if name == "nOpen" then nOpen = val end
+    i = i + 1
+until i > 20
+nOpen = nOpen or fs.open
+
+term.clear()
+local f = nOpen("/fw/assets/err.nfp", "r")
+if f then
+    local img = paintutils.parseImage(f.readAll())
+    f.close()
+    paintutils.drawImage(img, 2, 2)
+end
+term.setTextColor(colors.white)
+term.setCursorPos(1, 8)
+term.write("No valid operating system could be found.")
+term.setCursorPos(1, 10)
+term.write("Please install any operating system or make valid /boot.img .")
+term.setCursorPos(1,12)
+term.write("For more help, visit our help page:")
+term.setTextColor(colors.red)
+term.setCursorPos(1,13)
+term.write("https://wiki.gghjk.net/cs/gvb/405")
+
+while true do
+    os.pullEventRaw()
+end
