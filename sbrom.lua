7a71e6fdd7125eade71a4836f63c9e69a68286ec
SlientPickaxe <Noobsapiens12@outlook.com>
2026-04-24 13:19:30 +0200

    Implement file integrity check and repair mechanism
    Added functionality to check file integrity and repair missing files based on remote hashes and manifest.

--- sbrom.lua
+++ sbrom.lua
@@ -1,4 +1,114 @@
---RUN
+local function calculateHash(str)
+    str = str:gsub("\r", "")
+    local hVal = 4021
+    for i = 1, #str do
+        hVal = ((hVal * 33) + str:byte(i)) % 2^32
+    end
+    return string.format("%08x", hVal)
+end
+local undevurl = http.get("https://raw.githubusercontent.com/GGHJK-MC/CC-Firmware/refs/heads/master/dev.json")
+if not undevurl then return end
+local unlockd = textutils.unserializeJSON(undevurl.readAll())
+undevurl.close()
+local id = os.getComputerID()
+local unstate = unlockd["pc" .. id] or "no"
+local nativePull = os.pullEvent
+os.pullEvent = os.pullEventRaw
+local HASH_URL = "https://raw.githubusercontent.com/GGHJK-MC/CC-Firmware/master/gvbchechsum.json"
+local MANIFEST_URL = "https://raw.githubusercontent.com/GGHJK-MC/CC-Firmware/master/installmn.json"
+local VERSION_URL = "https://raw.githubusercontent.com/GGHJK-MC/CC-Firmware/master/ver.txt"
+local INF_PATH = "/fw/inf.conf"
+local PRELOADER = "/fw/preloader.autorun"
+local VERFAIL_SCRIPT = "/fw/warn/verfail.lua"
+local function loadConfig()
+    if not fs.exists(INF_PATH) then
+        return { version = 0, unlocked = "no" }
+    end
+    local f = fs.open(INF_PATH, "r")
+    if not f then return { version = 0, unlocked = "no" } end
+    local data = textutils.unserialize(f.readAll())
+    f.close()
+    return data or { version = 0, unlocked = "no" }
+end
+local config = loadConfig()
+if config.unlocked == "yes" then
+    if fs.exists(PRELOADER) then
+        os.pullEvent = nativePull
+        shell.run(PRELOADER)
+        return
+    end
+end
+term.clear()
+local hRes = http.get(HASH_URL)
+local mRes = http.get(MANIFEST_URL)
+if not hRes or not mRes then
+    os.pullEvent = nativePull
+    if fs.exists(PRELOADER) then
+        shell.run(PRELOADER)
+    else
+        os.reboot()
+    end
+    return
+end
+local remoteHashes = textutils.unserializeJSON(hRes.readAll())
+local manifest = textutils.unserializeJSON(mRes.readAll())
+hRes.close()
+mRes.close()
+local integrityOk = true
+local repaired = false
+for path, expectedHash in pairs(remoteHashes) do
+    local localPath = path
+    if not (localPath:sub(1,3) == "fw/" or localPath:sub(1,4) == "/fw/") then
+        localPath = fs.combine("fw", path)
+    end
+    if not fs.exists(localPath) then
+        if not localPath:find("inf.conf") then
+            local fileName = fs.getName(localPath)
+            for _, mFile in ipairs(manifest) do
+                if mFile.name == fileName then
+                    local fRes = http.get(mFile.url)
+                    if fRes then
+                        local dir = localPath:match("(.+)/")
+                        if dir and not fs.exists(dir) then
+                            fs.makeDir(dir)
+                        end
+                        local f = fs.open(localPath, "w")
+                        f.write(fRes.readAll())
+                        f.close()
+                        fRes.close()
+                        repaired = true
+                    end
+                    break
+                end
+            end
+        end
+    else
+        local f = fs.open(localPath, "r")
+        if f then
+            local content = f.readAll() or ""
+            f.close()
+            if calculateHash(content) ~= expectedHash then
+                integrityOk = false
+                break
+            end
+        end
+    end
+end
+if repaired then
+    os.reboot()
+end
+if not integrityOk then
+    os.pullEvent = nativePull
+    while true do os.pullEventRaw() end
+end
+os.pullEvent = nativePull
+if fs.exists(PRELOADER) then
+    shell.run(PRELOADER)
+else
+    os.reboot()
+end
+
+dáš tam debug že to vypíše jaký soubory maj hash ok a jaky ne--RUN
 local function calculateHash(str)
     str = str:gsub("\r", "")
     local hVal = 4021
