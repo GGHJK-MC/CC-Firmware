3c10a88532d4ac4eaef56d211817d031726bd6db
SlientPickaxe <Noobsapiens12@outlook.com>
2026-04-24 17:14:24 +0200

    Update sbrom.lua

--- sbrom.lua
+++ sbrom.lua
@@ -26,7 +26,6 @@ if fs.exists(socketPath) then
         end
     end
 end
-
 local undevurl = http.get("https://raw.githubusercontent.com/GGHJK-MC/CC-Firmware/refs/heads/master/dev.json")
 if not undevurl then return end
 local unlockd = textutils.unserializeJSON(undevurl.readAll())
@@ -35,14 +34,12 @@ local id = os.getComputerID()
 local unstate = unlockd["pc" .. id] or "no"
 local nativePull = os.pullEvent
 os.pullEvent = os.pullEventRaw
-
 local HASH_URL = "https://raw.githubusercontent.com/GGHJK-MC/CC-Firmware/master/gvbchechsum.json"
 local MANIFEST_URL = "https://raw.githubusercontent.com/GGHJK-MC/CC-Firmware/master/installmn.json"
 local VERSION_URL = "https://raw.githubusercontent.com/GGHJK-MC/CC-Firmware/master/ver.txt"
 local INF_PATH = "/fw/inf.conf"
 local PRELOADER = "/fw/preloader.autorun"
 local VERFAIL_SCRIPT = "/fw/warn/verfail.lua"
-
 local function loadConfig()
     if not fs.exists(INF_PATH) then
         return { version = 0, unlocked = "no" }
@@ -53,7 +50,6 @@ local function loadConfig()
     f.close()
     return data or { version = 0, unlocked = "no" }
 end
-
 local config = loadConfig()
 if config.unlocked == "yes" then
     if fs.exists(PRELOADER) then
@@ -62,12 +58,7 @@ if config.unlocked == "yes" then
         return
     end
 end
-
 term.clear()
-term.setCursorPos(1, 1)
-print("[ CC-Firmware integrity check ]")
-print("")
-
 local hRes = http.get(HASH_URL)
 local mRes = http.get(MANIFEST_URL)
 if not hRes or not mRes then
@@ -79,35 +70,19 @@ if not hRes or not mRes then
     end
     return
 end
-
 local remoteHashes = textutils.unserializeJSON(hRes.readAll())
 local manifest = textutils.unserializeJSON(mRes.readAll())
 hRes.close()
 mRes.close()
-
 local integrityOk = true
 local repaired = false
-
--- DEBUG: počítadla
-local countOk = 0
-local countFail = 0
-local countMissing = 0
-local countSkipped = 0
-
 for path, expectedHash in pairs(remoteHashes) do
     local localPath = path
     if not (localPath:sub(1,3) == "fw/" or localPath:sub(1,4) == "/fw/") then
         localPath = fs.combine("fw", path)
     end
-
     if not fs.exists(localPath) then
         if not localPath:find("inf.conf") then
-            -- soubor chybí, zkusíme opravit
-            term.setTextColor(colors.yellow)
-            print("  MISSING  " .. localPath)
-            term.setTextColor(colors.white)
-            countMissing = countMissing + 1
-
             local fileName = fs.getName(localPath)
             for _, mFile in ipairs(manifest) do
                 if mFile.name == fileName then
@@ -121,71 +96,36 @@ for path, expectedHash in pairs(remoteHashes) do
                         f.write(fRes.readAll())
                         f.close()
                         fRes.close()
-                        term.setTextColor(colors.lime)
-                        print("  REPAIRED " .. localPath)
-                        term.setTextColor(colors.white)
                         repaired = true
                     end
                     break
                 end
             end
-        else
-            -- inf.conf přeskočíme
-            term.setTextColor(colors.gray)
-            print("  SKIPPED  " .. localPath)
-            term.setTextColor(colors.white)
-            countSkipped = countSkipped + 1
         end
     else
         local f = fs.open(localPath, "r")
         if f then
             local content = f.readAll() or ""
             f.close()
-            local actualHash = calculateHash(content)
-            if actualHash == expectedHash then
-                term.setTextColor(colors.lime)
-                print("  OK       " .. localPath .. "  [" .. actualHash .. "]")
-                term.setTextColor(colors.white)
-                countOk = countOk + 1
-            else
-                term.setTextColor(colors.red)
-                print("  FAIL     " .. localPath)
-                print("    expected: " .. expectedHash)
-                print("    got:      " .. actualHash)
-                term.setTextColor(colors.white)
-                countFail = countFail + 1
+            if calculateHash(content) ~= expectedHash then
                 integrityOk = false
-                -- odstraň break pokud chceš vidět VŠECHNY chybné soubory
                 break
             end
         end
     end
 end
-
--- Shrnutí
-print("")
-print(string.format("OK: %d  FAIL: %d  MISSING: %d  SKIPPED: %d", countOk, countFail, countMissing, countSkipped))
-print("")
-
 if repaired then
-    print("Opraveno, rebootuji...")
-    sleep(2)
     os.reboot()
 end
-
 if not integrityOk then
-    term.setTextColor(colors.red)
-    print("Integrita selhala! System zamcen.")
-    term.setTextColor(colors.white)
     os.pullEvent = nativePull
     while true do os.pullEventRaw() end
 end
-
 os.pullEvent = nativePull
-print("Integrita OK, spoustim firmware...")
-sleep(1)
 if fs.exists(PRELOADER) then
     shell.run(PRELOADER)
 else
     os.reboot()
 end
+
+dáš tam debug že to vypíše jaký soubory maj hash ok a jaky ne
